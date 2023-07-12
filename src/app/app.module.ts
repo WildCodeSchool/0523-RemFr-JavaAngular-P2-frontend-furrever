d78077d6c114540b7688de466a5b3b7d5bdc5f9a
bbrowny <bruno.fernandes3789@gmail.com>
2023-07-12 08:52:49 +0200

    space

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,21 @@ import { DemoPipe } from './pipes/demo.pipe';
 import { DemoDirective } from './directives/demo.directive';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
-import {NgOptimizedImage} from "@angular/common";
+import { NgOptimizedImage } from '@angular/common';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, DemoComponent, DemoPipe, DemoDirective, NavbarComponent, HomeComponent, ProfileComponent, LoginComponent],
+  declarations: [
+    AppComponent,
+    DemoComponent,
+    DemoPipe,
+    DemoDirective,
+    NavbarComponent,
+    HomeComponent,
+    ProfileComponent,
+    LoginComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, NgOptimizedImage],
   providers: [],
   bootstrap: [AppComponent],
