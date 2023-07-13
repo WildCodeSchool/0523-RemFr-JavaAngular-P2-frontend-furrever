343e7b6eeaca4a5f43cfb6b02385268905d11ff4
KevinRoig <kevinroig@hotmail.com>
2023-07-13 18:34:37 +0200

    fix merge

--- src/app/models/Species.ts
+++ src/app/models/Species.ts
@@ -1,3 +1,3 @@
 export class Species {
-  constructor(public name: string) {}
+  constructor(public id: string, public name: string) {}
 }
--- src/app/services/api/api-species.service.ts
+++ src/app/services/api/api-species.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { map, Observable } from "rxjs";
+import { Observable } from "rxjs";
+import { Species } from "../../models/Species";
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class ApiSpeciesService {
 
   constructor(private http: HttpClient) { }
 
-  getSpeciesName(): Observable<any> {
-    return this.http.get<any>("http://localhost:8080/api/species");
+  getSpeciesName(): Observable<Species[]> {
+    return this.http.get<Species[]>("http://localhost:8080/api/species");
   }
 }
