e5fc7bc3603189ec19094973336ca6cfcd5e63d4
bbrowny <bruno.fernandes3789@gmail.com>
2023-07-19 12:51:24 +0200

    fix death code

--- src/app/pages/search-result/search-result.component.ts
+++ src/app/pages/search-result/search-result.component.ts
@@ -19,16 +19,4 @@ export class SearchResultComponent implements OnInit {
       this.petsitterPreviewList = response;
     });
   }
-
-  /*new PetsitterPreview(
-      "190fff3b-3664-42ab-b5d9-af54b6278c8f",
-      "Sammy",
-      "Osinski",
-      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis",
-      "assets/images/lion.jpg",
-      25,
-      4,
-      124
-    ),
-  */
 }
