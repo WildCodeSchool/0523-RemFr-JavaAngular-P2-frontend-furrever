350566c08c726db935cde43252d85fb9aa58a899
bbrowny <bruno.fernandes3789@gmail.com>
2023-07-20 22:43:30 +0200

    stash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgOptimizedImage } from "@angular/common";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./app-routing.module";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 import { DemoPipe } from "./pipes/demo.pipe";
 import { InitialPipe } from "./pipes/initial.pipe";
@@ -15,22 +16,21 @@ import { FooterComponent } from "./components/footer/footer.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { CarouselComponent } from "./components/carousel/carousel.component";
 import { CommentCardComponent } from "./components/comment-card/comment-card.component";
+import { SearchFormComponent } from "./components/forms/search-form/search-form.component";
+import { SubHeaderComponent } from "./components/sub-header/sub-header.component";
+import { SignupFormComponent } from "./components/forms/signup-form/signup-form.component";
+import { UserInfosComponent } from "./components/user-infos/user-infos.component";
+import { ServicesListComponent } from "./components/services-list/services-list.component";
+import { ServicesComponent } from "./components/services/services.component";
+import { LoginFormComponent } from "./components/forms/login-form/login-form.component";
+import { PetsitterPreviewComponent } from "./components/petsitter-preview/petsitter-preview.component";
 
 import { HomeComponent } from "./pages/home/home.component";
 import { ProfileComponent } from "./pages/profile/profile.component";
-import { HttpClientModule } from "@angular/common/http";
-import { SearchFormComponent } from "./components/forms/search-form/search-form.component";
 import { SearchResultComponent } from "./pages/search-result/search-result.component";
-import { PetsitterPreviewComponent } from "./components/petsitter-preview/petsitter-preview.component";
 import { LogInComponent } from "./pages/log-in/log-in.component";
-import { LoginFormComponent } from './components/forms/login-form/login-form.component';
-import { SignUpComponent } from './pages/sign-up/sign-up.component';
-import { SubHeaderComponent } from './components/sub-header/sub-header.component';
-import { SignupFormComponent } from './components/forms/signup-form/signup-form.component';
-import { PetsitterProfileComponent } from './pages/petsitter-profile/petsitter-profile.component';
-import { UserInfosComponent } from './components/user-infos/user-infos.component';
-import { ServicesListComponent } from './components/services-list/services-list.component';
-import { ServicesComponent } from './components/services/services.component';
+import { SignUpComponent } from "./pages/sign-up/sign-up.component";
+import { PetsitterProfileComponent } from "./pages/petsitter-profile/petsitter-profile.component";
 
 @NgModule({
   declarations: [
--- src/app/components/forms/login-form/login-form.component.ts
+++ src/app/components/forms/login-form/login-form.component.ts
@@ -1,7 +1,7 @@
-import {Component} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
-import {ApiCallService} from "../../../services/api/api-call.service";
-import {Router} from "@angular/router";
+import { Component } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
+import { ApiCallService } from "../../../services/api/api-call.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: "app-login-form",
@@ -14,12 +14,29 @@ export class LoginFormComponent {
     password: new FormControl(""),
   });
 
-  constructor(private apiCallService: ApiCallService, private route: Router) {
-  }
+  errors: string[] = [];
+
+  constructor(private apiCallService: ApiCallService, private route: Router, private activatedRoute: ActivatedRoute) {}
 
   sendLogin() {
-    this.apiCallService.loginRequest(this.login.getRawValue()).subscribe((token) => {
-      localStorage.setItem("authtoken", token);
-    });
+    this.errors = [];
+    this.validation(this.login.getRawValue().email, this.login.getRawValue().password);
+    if (this.errors.length <= 0) {
+      this.apiCallService.loginRequest(this.login.getRawValue()).subscribe(({ token }) => {
+        localStorage.setItem("authtoken", token);
+        this.route.navigate(["/"]);
+      });
+    }
+  }
+
+  validation(email: string | null, password: string | null) {
+    if (!email || !email.trim() || !email.match(/[a-z0-9_\-\.]+@[a-z0-9_\-\.]+\.[a-z]+/i)) {
+      this.errors.push("Votre adresse email n'est pas une adresse valide");
+    }
+    if (!password) {
+      this.errors.push("Votre mot de passe n'est pas un mot de passe valide");
+    }
   }
 }
+
+//!email.match(/[a-z0-9_\-\.]+@[a-z0-9_\-\.]+\.[a-z]+/i)
