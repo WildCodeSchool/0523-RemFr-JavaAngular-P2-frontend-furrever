15b09e7ca0a8316730acb8139c705df313cf13a2
bbrowny <bruno.fernandes3789@gmail.com>
2023-07-20 23:20:28 +0200

    last fix before dodo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,30 +16,23 @@ import { FooterComponent } from "./components/footer/footer.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { CarouselComponent } from "./components/carousel/carousel.component";
 import { CommentCardComponent } from "./components/comment-card/comment-card.component";
+import { LoaderComponent } from "./components/loader/loader.component";
+import { PetsitterPreviewComponent } from "./components/petsitter-preview/petsitter-preview.component";
+import { SearchFormComponent } from "./components/forms/search-form/search-form.component";
 import { UserInfosComponent } from "./components/user-infos/user-infos.component";
 import { ServicesListComponent } from "./components/services-list/services-list.component";
 import { ServicesComponent } from "./components/services/services.component";
-import { LoaderComponent } from "./components/loader/loader.component";
+import { ModalServiceComponent } from "./components/modal-service/modal-service.component";
+import { LoginFormComponent } from "./components/forms/login-form/login-form.component";
 import { SubHeaderComponent } from "./components/sub-header/sub-header.component";
 import { SignupFormComponent } from "./components/forms/signup-form/signup-form.component";
-import { LoginFormComponent } from "./components/forms/login-form/login-form.component";
-import { PetsitterPreviewComponent } from "./components/petsitter-preview/petsitter-preview.component";
-import { SearchFormComponent } from "./components/forms/search-form/search-form.component";
-import { UserInfosComponent } from './components/user-infos/user-infos.component';
-import { ServicesListComponent } from './components/services-list/services-list.component';
-import { ServicesComponent } from './components/services/services.component';
-import { ModalServiceComponent } from './components/modal-service/modal-service.component';
-import { LoginFormComponent } from './components/forms/login-form/login-form.component';
-import { SubHeaderComponent } from './components/sub-header/sub-header.component';
-import { SignupFormComponent } from './components/forms/signup-form/signup-form.component';
 
 import { HomeComponent } from "./pages/home/home.component";
 import { ProfileComponent } from "./pages/profile/profile.component";
 import { SearchResultComponent } from "./pages/search-result/search-result.component";
 import { LogInComponent } from "./pages/log-in/log-in.component";
-import { SignUpComponent } from './pages/sign-up/sign-up.component';
-import { PetsitterProfileComponent } from './pages/petsitter-profile/petsitter-profile.component';
-
+import { SignUpComponent } from "./pages/sign-up/sign-up.component";
+import { PetsitterProfileComponent } from "./pages/petsitter-profile/petsitter-profile.component";
 
 @NgModule({
   declarations: [
