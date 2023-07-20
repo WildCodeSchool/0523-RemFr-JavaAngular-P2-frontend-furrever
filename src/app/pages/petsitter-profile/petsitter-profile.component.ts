67a9b9665e1bf339add73549c68d61b0b57a7abe
KevinRoig <kevinroig@hotmail.com>
2023-07-20 10:38:16 +0200

    made request for petsitter profile

--- src/app/pages/petsitter-profile/petsitter-profile.component.ts
+++ src/app/pages/petsitter-profile/petsitter-profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { ApiCallService } from "../../services/api/api-call.service";
+import { PetsitterViewByOwner } from "../../models/PetsitterViewByOwner";
+import { Comment } from "../../models/Comment";
 import { Service } from "../../models/Service";
 
 @Component({
@@ -8,24 +10,22 @@ import { Service } from "../../models/Service";
   templateUrl: "./petsitter-profile.component.html",
   styleUrls: ["./petsitter-profile.component.scss"],
 })
-
 export class PetsitterProfileComponent implements OnInit {
-  constructor(private activatedRoute: ActivatedRoute, private apiCallService: ApiCallService) {
-  }
+  commentList: Comment[] = [];
+  petSitterProfile!: PetsitterViewByOwner;
+  serviceList: Service[] = [];
+  constructor(private activatedRoute: ActivatedRoute, private apiCallService: ApiCallService) {}
 
   ngOnInit() {
     const userId = this.activatedRoute.snapshot.params["id"];
     console.log(userId);
 
-    this.apiCallService.getPetsittersById(userId)
-      .subscribe((response: any) => console.log(response));
+    this.apiCallService
+      .getPetsittersById(userId)
+      .subscribe(({ commentTemplateList, petSitterProfile, serviceTemplateList }) => {
+        this.commentList = commentTemplateList;
+        this.petSitterProfile = petSitterProfile;
+        this.serviceList = serviceTemplateList;
+      });
   }
-
-  serviceList: Service[] = [
-    new Service("blablablablablabla", 25, "Bovin", "garde prolongée", null, null, true),
-    new Service("blablablablablabla", 25, "Bovin", "garde prolongée", null, null, true),
-    new Service("blablablablablabla", 25, "Bovin", "garde prolongée", null, null, true),
-    new Service("blablablablablabla", 25, "Bovin", "garde prolongée", null, null, true),
-  ];
 }
-
--- src/app/services/api/api-call.service.ts
+++ src/app/services/api/api-call.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { Observable, map } from "rxjs";
+import { Observable } from "rxjs";
 import { Comment } from "../../models/Comment";
 import { Species } from "../../models/Species";
 import { PetsitterPreview } from "../../models/PetsitterPreview";
 import { SearchRequest } from "../../models/SearchRequest";
 import { PetsitterViewByOwner } from "../../models/PetsitterViewByOwner";
+import { Service } from "../../models/Service";
 
 type ResponsePetSitter = {
-  commentTemplateList: Comment[],
-  petSitterProfile: PetsitterViewByOwner,
- // serviceTemplateList: Ser
-}
+  commentTemplateList: Comment[];
+  petSitterProfile: PetsitterViewByOwner;
+  serviceTemplateList: Service[];
+};
+
 @Injectable({
   providedIn: "root",
 })
@@ -22,7 +24,8 @@ export class ApiCallService {
   private endPointSearchPetsitter = "petsitters/search";
   private endPointGetPetSitterById = "petsitters/";
 
-  constructor(public http: HttpClient) {}
+  constructor(public http: HttpClient) {
+  }
 
   getTopComments(): Observable<Comment[]> {
     return this.http.get<Comment[]>(this.API_URL + this.endPointGetTopComments);
@@ -35,7 +38,8 @@ export class ApiCallService {
   getPetsitters(payload: SearchRequest): Observable<PetsitterPreview[]> {
     return this.http.post<PetsitterPreview[]>(this.API_URL + this.endPointSearchPetsitter, payload);
   }
-  getPetsittersById(id: string): any{
-    return this.http.get<any>(this.API_URL + this.endPointGetPetSitterById + id);
+
+  getPetsittersById(id: string): Observable<ResponsePetSitter> {
+    return this.http.get<ResponsePetSitter>(this.API_URL + this.endPointGetPetSitterById + id);
   }
 }
