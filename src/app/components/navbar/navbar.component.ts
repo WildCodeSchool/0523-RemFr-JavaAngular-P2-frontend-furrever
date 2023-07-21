cc1463a6fc9879a1f5e484ea1e6d2d07d6560987
bbrowny <bruno.fernandes3789@gmail.com>
2023-07-21 14:51:36 +0200

    test

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,24 @@
-import { Component } from "@angular/core";
+import {Component, OnChanges, OnInit} from "@angular/core";
+import jwtDecode from "jwt-decode";
+
 
 @Component({
   selector: "app-navbar",
   templateUrl: "./navbar.component.html",
   styleUrls: ["./navbar.component.scss"],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnChanges{
   isLog = false;
-
-  log() {
-    this.isLog = !this.isLog;
+  ngOnChanges(): void {
+    const token = localStorage.getItem("authtoken");
+    if (token) {
+      this.isLog = true;
+      const jwt = JSON.parse(token);
+      console.log('token parse', jwt)
+      console.log('jwt decode', jwtDecode(jwt))
+    }
+    this.isLog = false;
   }
+
+
 }
