69a64c7cb1dd5697b702f1d31ad3450bbead6675
bbrowny <bruno.fernandes3789@gmail.com>
2023-07-27 00:18:54 +0200

    add error is not log

--- src/app/components/forms/login-form/login-form.component.ts
+++ src/app/components/forms/login-form/login-form.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from "@angular/core";
-import { FormControl, FormGroup } from "@angular/forms";
-import { ApiCallService } from "../../../services/api/api-call.service";
-import { ActivatedRoute, Router } from "@angular/router";
-import { Observable } from "rxjs";
-import { Store } from "@ngrx/store";
-import { addUserOnStore, removeUserOnStore } from "../../../services/state/userStore.actions";
-import { AuthService } from "../../../services/auth/auth.service";
+import {Component, OnInit} from "@angular/core";
+import {FormControl, FormGroup} from "@angular/forms";
+import {ApiCallService} from "../../../services/api/api-call.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Observable} from "rxjs";
+import {Store} from "@ngrx/store";
+import {addUserOnStore, removeUserOnStore} from "../../../services/state/userStore.actions";
+import {AuthService} from "../../../services/auth/auth.service";
 import jwtDecode from "jwt-decode";
-import { PayloadToken } from "../../../models/PayloadToken";
-import { ToastrService } from "ngx-toastr";
+import {PayloadToken} from "../../../models/PayloadToken";
+import {ToastrService} from "ngx-toastr";
+
 
 @Component({
   selector: "app-login-form",
@@ -31,7 +32,8 @@ export class LoginFormComponent implements OnInit {
     private store: Store<{ userStore: boolean }>,
     private authService: AuthService,
     private toastr: ToastrService
-  ) {}
+  ) {
+  }
 
   ngOnInit(): void {
     if (this.authService.isConnectedVerif()) {
@@ -46,17 +48,20 @@ export class LoginFormComponent implements OnInit {
     this.errors = [];
     this.validation(this.login.getRawValue().email, this.login.getRawValue().password);
     if (this.errors.length <= 0) {
-      this.apiCallService.loginRequest(this.login.getRawValue()).subscribe(({ token }) => {
-        localStorage.setItem("authToken", token);
-        const payloadToken: PayloadToken = jwtDecode(token);
-        this.store.dispatch(addUserOnStore({ picture: payloadToken.picture }));
-        const queryParam = this.activatedRoute.snapshot.queryParams;
-        this.toastr.success("Vous êtes connecté !");
-        if (queryParam["id"]) {
-          this.route.navigate(["profile-petsitter/" + queryParam["id"]]);
-        } else {
-          this.route.navigate(["/profile"]);
-        }
+      this.apiCallService.loginRequest(this.login.getRawValue()).subscribe({
+        next: ({ token }) => {
+          localStorage.setItem("authToken", token);
+          const payloadToken: PayloadToken = jwtDecode(token);
+          this.store.dispatch(addUserOnStore({ picture: payloadToken.picture }));
+          const queryParam = this.activatedRoute.snapshot.queryParams;
+          this.toastr.success("Vous êtes connecté !");
+          if (queryParam["id"]) {
+            this.route.navigate(["profile-petsitter/" + queryParam["id"]]);
+          } else {
+            this.route.navigate(["/profile"]);
+          }
+        },
+        error: () => this.errors.push("Votre mot de passe et ou votre email sont incorrectes."),
       });
     }
   }
