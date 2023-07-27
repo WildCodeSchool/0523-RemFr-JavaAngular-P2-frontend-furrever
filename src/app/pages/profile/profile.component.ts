d2c940ac984677fe8632c05c9031c42d922ec5fc
bbrowny <bruno.fernandes3789@gmail.com>
2023-07-27 20:08:46 +0200

    add view comment on petsitter profile

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -5,18 +5,19 @@ import { Animal } from "../../models/Animal";
 import { ApiCallService } from "../../services/api/api-call.service";
 import { UserProfile } from "../../models/UserProfile";
 import { Service } from "../../models/Service";
+import { Comment } from "../../models/Comment";
 
 @Component({
   selector: "app-profile",
   templateUrl: "./profile.component.html",
   styleUrls: ["./profile.component.scss"],
 })
-
 export class ProfileComponent implements OnInit {
   animalList: Animal[] = [];
   user!: UserProfile;
   nbPendingTransactions = 0;
   serviceList: Service[] = [];
+  commentList: Comment[] = [];
   constructor(private authService: AuthService, private route: Router, private apiCallService: ApiCallService) {}
 
   ngOnInit(): void {
@@ -26,9 +27,9 @@ export class ProfileComponent implements OnInit {
     this.apiCallService.getCurrentUser().subscribe((profile) => {
       this.animalList = profile.animalTemplateList;
       this.user = profile.userProfile;
-      console.log(profile.serviceTemplateList);
       this.nbPendingTransactions = profile.nbPendingTransactions;
       this.serviceList = profile.serviceTemplateList;
+      this.commentList = profile.commentTemplateList;
     });
   }
 }
